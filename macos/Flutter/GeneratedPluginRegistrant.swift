62e463fc461a2ccb5dc660095fb1bceba5164448
UTKARSH SINGH <usjadon19@gmail.com>
2024-07-04 14:59:15 +0530

    updated July 4, 2024

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,7 @@ import Foundation
 
 import geolocator_apple
 import package_info_plus
-import path_provider_macos
+import path_provider_foundation
 import share_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
